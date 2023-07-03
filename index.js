a3192ddf4ea095c89addd87eb81bb256d47ecbfb
Bret Barden <bret.barden@gmail.com>
2023-07-02 21:26:33 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,42 @@
+// Test 1 //
+function add(a, b) {
+    return a + b;
+}
+
+// Test 2 //
+function subtract(a, b) {
+    return a - b;
+}
+
+// Test 3 //
+function multiply(a, b) {
+    return a * b;
+}
+
+// Test 4 //
+function divide(a, b) {
+    return a / b;
+}
+
+// Tests 9 and 10 //
+
+function increment(n) {
+  return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+  }
+
+  // Tests 11+//
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+makeInt("Brittany");
+
+  
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+preserveDecimnal(12.874);
+preserveDecimnal("Janet");
